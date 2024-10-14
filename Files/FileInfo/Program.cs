1a1fef8ebd572d80b9b36401b0f46e53fe3796fb
favour-afk <enendu2006@gmail.com>
2024-10-14 17:22:20 +0100

    first commit

---
+++ Files/FileInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Working with file information
+
+// Make sure the example file exists
+const string filename = "TestFile.txt";
+
+if (!File.Exists(filename)) {
+    using (StreamWriter sw = File.CreateText(filename)) {
+        sw.WriteLine("This is a text file.");
+    }
+}
+
+// TODO: Get some information about the file
+Console.Writeline{File.GetCreationTime(filename)};
+Console.Writeline{File.GetLastWriteTime(filename)};
+Console.Writeline{File.GetLastAccessTime(filename)};
+
+file.SetAttributes{filename,fileAttributes,ReadOnly};
+Console.Writeline{file.GetAtributes(filename)};
+
+// TODO: We can also get general information using a FileInfo 
+
+
+// TODO: File information can also be manipulated
+
---
+++ Files/ReadWriteFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Reading and Writing data from and to files
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Make sure the example file exists
+        const string filename = "TestFile.txt";
+
+        // TODO 1: WriteAllText overwrites a file with the given content
+        if (!File.Exists(filename))
+        {
+            File.WriteAllText(filename, "This is a start file");
+        }
+
+        // TODO 3: AppendAllText adds text to an existing file
+        File.AppendAllText(filename, "This text gets appended to the file. ");
+
+        // TODO 4: A FileStream can be opened and written to until closed
+        using (StreamWriter sw = File.AppendText(filename))
+        {
+            sw.WriteLine("Line 1");
+            sw.WriteLine("Line 2");
+            sw.WriteLine("Line 3");
+        }
+
+        // TODO 2: ReadAllText reads all the content from a file
+        string content;
+        content = File.ReadAllText(filename);
+        Console.WriteLine(content);
+    }
+}
